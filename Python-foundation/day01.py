4558baa9ab4514715f5027c849a0078804fcf22f
AtharvaG1001 <gaikwadatharva1001@gmail.com>
2026-03-10 19:34:40 +0530

    day01 - variable and data type

--- Python-foundation/day01.py
+++ Python-foundation/day01.py
@@ -4,7 +4,8 @@ model_name = "my_model"
 is_training = True
 
 
-print(learning_rate)
-print(epochs)
-print(model_name)
-print(is_training)
+print("learning_rate =",learning_rate, " type:", type(learning_rate))
+print("epochs =",epochs, "type:", type(epochs))
+print("model_name = ",model_name, "type:", type(model_name))
+print("is_training =",is_training, "type:", type(is_training))
+# Output:
