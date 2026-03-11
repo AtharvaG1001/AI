39b6154bee8d053cc99b920a50b475b28695b2c3
AtharvaG1001 <gaikwadatharva1001@gmail.com>
2026-03-11 14:26:19 +0530

    day02 - 10 for loop

--- Python-foundation/day02-app.py
+++ Python-foundation/day02-app.py
@@ -3,17 +3,72 @@ learning_rate = 0.1
 gradient = 0.5
 
 weight =(weight - learning_rate * gradient)
-if weight < 0:print("updated_weight:",weight)
-else: print("no weight")
+print("updated_weight:",weight)
+if weight < 0.5:
+    print("warning")
+else:
+    print("weight normal")
 
 weight = ( weight - learning_rate * gradient) 
 print("updated_weight:",weight)
+if weight < 0.5:
+    print("warning")
+else:
+    print("weight normal")
 
 weight = (weight - learning_rate * gradient)
 print("updated_weight:",weight)
+if weight < 0.5:
+    print("warning")
+else:
+    print("weight normal")
 
 weight = (weight - learning_rate * gradient)
 print("updated_weight:",weight)
+if weight < 0.5:
+    print("warning")
+else:
+    print("weight normal")
 
+weight = (weight - learning_rate * gradient)
+print("updated_weight:",weight)
+if weight < 0.5:
+    print("warning")
+else:
+    print("weight normal")
 
+weight = (weight - learning_rate * gradient)
+print("updated_weight:",weight)
+if weight < 0.5:
+    print("warning")
+else:
+    print("weight normal")
+
+weight = (weight - learning_rate * gradient)
+print("updated_weight:",weight)
+if weight < 0.5:
+    print("warning")
+else:
+    print("weight normal")
+
+weight = (weight - learning_rate * gradient)
+print("updated_weight:",weight)
+if weight < 0.5:
+    print("warning")
+else:
+    print("weight normal")
+
+weight = (weight - learning_rate * gradient)
+print("updated_weight:",weight)
+if weight < 0.5:
+    print("warning")
+else:
+    print("weight normal")
+
+weight = (weight - learning_rate * gradient)
+print("updated_weight:",weight)
+if weight < 0.5:
+    print("warning")
+else:
+    print("weight normal")
 
