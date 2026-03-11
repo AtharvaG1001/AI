0b5c4db5bf3b7003e77d1fa28e75254cb870f0ba
AtharvaG1001 <gaikwadatharva1001@gmail.com>
2026-03-11 12:30:54 +0530

    day02 - aritmatic operations

---
+++ Python-foundation/day02.py
@@ -0,0 +1,18 @@
+x = 10
+y = 3
+
+
+print("add:", x + y)  #Addition
+print("subtract:", x - y)  #substraction
+print("multipy:", x * y)  #multiplication
+print("divide:", x / y)  #division
+print("floor clear:", x // y) #floor clear
+print("module:", x % y)  #module
+print("power:", x ** y) #power
+
+
+weight = 1
+learning_rate = 0.001
+weight = weight - learning_rate
+print("updated weight:",weight)
+print("int value of weight:", int(weight))
