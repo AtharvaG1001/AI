1c300b81d8bf620fd2713fd8784e84caf1ed935b
AtharvaG1001 <gaikwadatharva1001@gmail.com>
2026-03-12 12:32:23 +0530

    day03-comand operaters

---
+++ Python-foundation/day03.py
@@ -0,0 +1,14 @@
+loss = 0.8
+
+if loss > 0.5:
+    print("loss is high - keep training")
+elif loss > 0.2:
+    print("loss is acceptable ")
+else:    print("loss is low - model is converging")
+
+# >    # greater than
+# <    # less than
+# >=   # greater than or equal
+# <=   # less than or equal
+# ==   # equal to (NOT assignment)
+# !=   # not equal to
